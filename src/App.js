ed448d3f52075a5f83e0f64304966120c383f0fc
SerhiiHrynchyshyn <server_059@ukr.net>
2022-07-25 22:08:57 +0300

    hw-2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-        <h1>hello</h1>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
 
 export default App;
